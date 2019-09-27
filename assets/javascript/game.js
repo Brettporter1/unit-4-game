64e6b69c081903237bb49cc544563a5634dc229d
Brett Porter <gkdesign06@gmail.com>
2019-09-27 01:23:07 -0500

    Added basic structure to page and defined my characters within an object

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var characters = {
+    "Johny C":{
+        name:"Johny C",
+        health:270,
+        attack:21
+    },
+    "Jamie B":{
+        name:"Jamie B",
+        health:290,
+        attack:18
+    },
+    "Frankie":{
+        name:"Frankie",
+        health:430,
+        attack:9
+    },
+    "Jay-Bel":{
+        name:"Jay-Bel",
+        health:340,
+        attack:13,
+        imgUrl:'../images/'
+    }
+}
+console.log(characters);
+
+// Put characters on the DOM in initial spot to start the game
+var chosefrom = function(char, location){
+    if(location === "#characterContainer"){
+        $
+    }
+}
+
+
+
+// move selected character to its location
+
+
+
+// move selected opponent to their location
