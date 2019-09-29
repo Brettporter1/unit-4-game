2ef57efb38d8e11f181b8347c5ba322943d68665
Brett Porter <gkdesign06@gmail.com>
2019-09-29 12:21:26 -0500

    added win and lose screens and restart buttons to reload the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,25 @@ function startgame(){
     var characters = {
         "Johny-C": {
             name: "Johny-C",
-            health: 4270,
-            attack: 21,
+            health: 490,
+            attack: 25,
             mugShot: "./assets/images/Johnny-Cash.jpg"
         },
         "Jamie-B": {
             name: "Jamie-B",
-            health: 4290,
+            health: 490,
             attack: 18,
             mugShot: "./assets/images/angleincidence-800x511.jpg"
         },
         "Frankie": {
             name: "Frankie",
-            health: 4430,
+            health: 630,
             attack: 9,
             mugShot: "./assets/images/BlueEyes.jpg"
         },
         "Jay-Bel": {
             name: "Jay-Bel",
-            health: 4340,
+            health: 540,
             attack: 13,
             mugShot: "./assets/images/johnBelushi.jpg"
         }
@@ -30,16 +30,15 @@ function startgame(){
     var theChosenOne = '';
     var brawlChoices =[];
     var brawler= '';
-    var healthCount;
     var winCount = 0
     var attackCount = 1
 
     var showCharacter = function (character, pageLocation, charType) {
         console.log(character);
-        var playerDiv = $("<div class='character'>").attr('id', character.name);
-        var playerName = $('<h4>').text(character.name);
+        var playerDiv = $(`<div class="character"id=${character.name}>`);
+        var playerName = $(`<h4>${character.name}</h4>`);
         var playerImg = $(`<img class="character-image" src="${character.mugShot}">`);
-        var playerHealth = $('<h4>').text(character.health);
+        var playerHealth = $(`<h4>${character.health}</h4>`);
         $(pageLocation).append(playerDiv);
         $(playerDiv).append(playerName).append(playerImg).append(playerHealth);
 
@@ -55,7 +54,16 @@ function startgame(){
         }
     }
 var winner = function(){
-    $('#versus').hide();
+    $('.mainGameContainer').hide();
+    showCharacter(theChosenOne,'#winner', 'chosen');
+    $('.chosenWinner').toggleClass('show hide');
+}
+
+var loser = function(){
+    $('.mainGameContainer').hide();
+    playerHealth = 'Dead';
+    showCharacter(theChosenOne,'#loser', 'chosen');
+    $('.chosenLoser').toggleClass('show hide');
 }
 // Put characters on the DOM in initial spot to start the game
 var showAllCharacters = function (charObject, location) {
@@ -113,12 +121,12 @@ var showAllCharacters = function (charObject, location) {
     })
     $('#attackBtn').on('click', function(){
         if($('#brawlBuddy').children().length !== 0){
-            brawler.health -= (theChosenOne.attack * attackCount);
+            brawler.health -= Math.floor((Math.random()*theChosenOne.attack) * attackCount);
             if(brawler.health >= 0){
                 showAllCharacters(brawler, 'theirDamage');
-                theChosenOne.health -= (brawler.attack + attackCount);
+                theChosenOne.health -= Math.floor((Math.random()*brawler.attack) * attackCount);
                 if(theChosenOne.health <= 0){
-                    showAllCharacters
+                    loser();
                 }
                 showAllCharacters(theChosenOne,'myDamage');
             }
@@ -127,16 +135,18 @@ var showAllCharacters = function (charObject, location) {
                 winCount++
                 console.log(winCount)
                 if(winCount >= 3){
-                showAllCharacters(theChosenOne, 'winner')
+                    console.log('winner');
+                winner();
                 }
             }
         }
         attackCount++;
     })
+    $('#winnerPlayAgain').on('click',function(){
+        location.reload(true);
+    });
+    $('#loserPlayAgain').on('click',function(){
+        location.reload(true);
+    });
 };
 startgame();
-// move selected character to its location
-
-
-
-// move selected opponent to their location
